16a5443a50d01aae441de683ce5e7dd5058c6e1a
Adam Christie <fractos@gmail.com>
2017-03-10 15:53:07 +0000

    some fixes for adding the text and manifest queues. also removing the run in background-ness from the run_starsky_ingest script.

--- run_container_example.sh
+++ run_container_example.sh
@@ -3,9 +3,11 @@
 docker run \
         --env AWS_ACCESS_KEY_ID="" \
         --env AWS_SECRET_ACCESS_KEY=""  \
-		--env STARSKY_ERROR_QUEUE=starsky-error-queue \
-		--env STARSKY_TEXT_METADATA_BUCKET=starsky-text-meta \
-		--env STARSKY_INDEX_BUCKET=starsky-index \
-		--env STARSKY_INGEST_QUEUE=starsky-ingest-queue \
-		--env STARSKY_AWS_REGION=eu-west-1 \
+	--env STARSKY_ERROR_QUEUE=starsky-error-queue \
+	--env STARSKY_TEXT_METADATA_BUCKET=starsky-text-meta \
+	--env STARSKY_INDEX_BUCKET=starsky-index \
+	--env STARSKY_INGEST_QUEUE=starsky-ingest-queue \
+	--env STARSKY_TEXT_QUEUE=starsky-text-queue \
+	--env STARSKY_MANIFEST_QUEUE=starsky-manifest-queue \
+	--env STARSKY_AWS_REGION=eu-west-1 \
         starsky
