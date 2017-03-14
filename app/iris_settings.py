011a7fc0ba3926b5dab1889f82521f0adea6f85d
John <john-root@users.noreply.github.com>
2017-03-14 14:29:55 +0000

    moved iris config into environment variables

--- app/iris_settings.py
+++ app/iris_settings.py
@@ -1,4 +1,5 @@
-AWS_REGION = "us-west-1"
-IRIS_SNS_TOPIC = "iris-test-topic"
-IRIS_SQS_APP_QUEUE = "iris-starsky-queue"
+import os
+AWS_REGION = REGION = os.environ.get('IRIS_AWS_REGION')  # "us-west-1"
+IRIS_SNS_TOPIC = os.environ.get('IRIS_TOPIC')  # "iris-test-topic"
+IRIS_SQS_APP_QUEUE = os.environ.get('IRIS_QUEUE')  # "iris-starsky-queue"
 IRIS_POLL_INTERVAL = 20
